304b138f9da966367978179301a8bb4990995d74
Mad Max <maximiliano.bordon@gmail.com>
2020-05-04 17:13:46 -0300

    Create V20__Create_customer20.sql

---
+++ basededatos/V20__Create_customer20.sql
@@ -0,0 +1,26 @@
+CREATE TABLE ClientePrueba22(
+
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);
+
+
+
+CREATE TABLE ClientePrueba23(
+
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);
+
+
+
+
+CREATE TABLE ClientePrueba21(
+
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);
+© 2020 GitHub, In

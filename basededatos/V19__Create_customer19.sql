09e5debfd1225e927a35a3e5e9e9d28391b291f3
Mad Max <maximiliano.bordon@gmail.com>
2020-05-04 17:05:06 -0300

    Update V19__Create_customer19.sql

--- basededatos/V19__Create_customer19.sql
+++ basededatos/V19__Create_customer19.sql
@@ -18,25 +18,9 @@ last_name  VARCHAR(50)
 
 
 
-/*CREATE TABLE ClientePrueba18(
-
-first_name VARCHAR(50),
-last_name  VARCHAR(50)
-
-);*/
-
-
-
 CREATE TABLE ClientePrueba21(
 
 first_name VARCHAR(50),
 last_name  VARCHAR(50)
 
 );
-
-/*CREATE TABLE ClientePrueba22(
-
-first_name VARCHAR(50),
-last_name  VARCHAR(50)
-
-);*/

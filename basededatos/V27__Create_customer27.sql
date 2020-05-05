e1146beff884b73e9a482bbaa70af6f59d47680b
Mad Max <maximiliano.bordon@gmail.com>
2020-05-05 18:03:14 -0300

    Create V27__Create_customer27.sql

---
+++ basededatos/V27__Create_customer27.sql
@@ -0,0 +1,11 @@
+
+insert into ClientePrueba40 values(1,'a','b'); commit;
+insert into ClientePrueba40 values(1,'b','c');commit;
+
+CREATE TABLE ClientePrueba40(
+
+numero INTEGER PRIMARY KEY,
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);

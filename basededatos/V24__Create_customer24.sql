5382a667c4e881d4514f3e4838b25124f5a6fe0f
Mad Max <maximiliano.bordon@gmail.com>
2020-05-05 14:45:10 -0300

    Create V24__Create_customer24.sql

---
+++ basededatos/V24__Create_customer24.sql
@@ -0,0 +1,22 @@
+CREATE TABLE ClientePrueba30(
+
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);
+
+
+CREATE TABLE ClientePrueba31(
+
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);
+
+
+CREATE TABLE ClientePrueba32(
+
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);

10666a9a1b285dceced4b45dc823abd53450dac3
Mad Max <maximiliano.bordon@gmail.com>
2020-05-06 15:53:00 -0300

    Create V28__Create_customer28.sql

---
+++ basededatos/V28__Create_customer28.sql
@@ -0,0 +1,25 @@
+
+
+CREATE TABLE ClientePrueba50(
+
+numero INTEGER PRIMARY KEY,
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);
+
+CREATE TABLE ClientePrueba51(
+
+numero INTEGER PRIMARY KEY,
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);
+
+CREATE TABLE ClientePrueba52(
+
+numero INTEGER PRIMARY KEY,
+first_name VARCHAR(50),
+last_name  VARCHAR(50)
+
+);
